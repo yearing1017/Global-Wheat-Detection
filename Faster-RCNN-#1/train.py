f986c7c6b1b5bcefc2894c03d40adeecc0d8a8d0
yearing1017 <33566006+yearing1017@users.noreply.github.com>
2020-06-29 17:56:39 +0800

    Update train.py

--- Faster-RCNN-#1/train.py
+++ Faster-RCNN-#1/train.py
@@ -6,6 +6,7 @@ from torchvision.models.detection import FasterRCNN
 from torchvision.models.detection.rpn import AnchorGenerator
 
 from WheatData import train_data_loader, valid_data_loader
+from evaluate import *
 
 class Averager:
     def __init__(self):
@@ -43,18 +44,19 @@ def train(epoch=200):
 
     model.to(device)
     params = [p for p in model.parameters() if p.requires_grad]
-    #optimizer = torch.optim.Adam(params, lr=1e-3)
-    optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
+    optimizer = torch.optim.Adam(params, lr=1e-3)
+    #optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
     lr_scheduler = None
 
     loss_hist = Averager()
-    val_loss_hist = Averager()
-    itr = 1
-    least_loss = float('inf')
+    valid_pred_min = 0.5
+    #val_loss_hist = Averager()
+    #least_loss = float('inf')
 
     for epo in range(epoch):
+        itr = 1
         loss_hist.reset()
-        val_loss_hist.reset()
+        #val_loss_hist.reset()
         model.train()
         for images, targets, image_ids in train_data_loader:
             
@@ -74,29 +76,58 @@ def train(epoch=200):
             optimizer.step()
 
             if itr % 50 == 0:
+                line_1 = f"Iteration #{itr} loss: {loss_value}"
                 print(f"Iteration #{itr} loss: {loss_value}")
-
+                with open('log/logs_frc_0629.txt', 'a') as f :
+                    f.write(line_1)
+                    f.write('\r\n')
             itr += 1
 
         # 验证
         model.eval()
+        validation_image_precisions = []
+        iou_thresholds = [x for x in np.arange(0.5, 0.76, 0.05)]
         with torch.no_grad():
             for val_images, val_targets, val_image_ids in valid_data_loader:
                 images = list(image.to(device) for image in val_images)
                 targets = [{k: v.to(device) for k, v in t.items()} for t in val_targets]
 
-                val_loss_dict = model(images, targets)  # Returns losses and detections
-                val_losses = sum(loss for loss in val_loss_dict.values())
-                loss_value = losses.item()
-                loss_hist.send(loss_value)
+                outputs = model(images)
+                for i, image in enumerate(images):
+                    boxes = outputs[i]['boxes'].data.cpu().numpy()
+                    scores = outputs[i]['scores'].data.cpu().numpy()
+                    gt_boxes = targets[i]['boxes'].cpu().numpy()
+                    # np.argsort(scores)返回scores从小到大的索引；[::-1]表示倒序，从大到小
+                    preds_sorted_idx = np.argsort(scores)[::-1]
+                    preds_sorted = boxes[preds_sorted_idx]
+                    image_precision = calculate_image_precision(preds_sorted,
+                                                        gt_boxes,
+                                                        thresholds=iou_thresholds,
+                                                        form='pascal_voc')
+                    validation_image_precisions.append(image_precision)
+        valid_prec = np.mean(validation_image_precisions)
+        line_2 = "Validation IOU: {0:.4f}".format(valid_prec)
+        print("Validation IOU: {0:.4f}".format(valid_prec))
+        with open('log/logs_frc_0629.txt', 'a') as f :
+            f.write(line_2)
+            f.write('\r\n')
 
-        # 判断是否为更优的模型，以loss为标准
-        if val_loss_hist.value<least_loss:
-            least_loss = val_loss_hist.value
-            lval=int(least_loss*1000)/1000
-            torch.save(model.state_dict(), f'fasterrcnn_custom_test_ep{epo}_loss{lval}.pth')
-            
-        else:
-            if lr_scheduler is not None:
-                lr_scheduler.step()
-        print(f"Epoch #{epo} train_loss: {loss_hist.value} val_loss: {val_loss_hist.value}")
+        # 判断是否为更优的模型
+        if valid_prec >= valid_pred_min:
+            line_3 = 'Validation precision increased({:.6f} --> {:.6f}).  Saving model ...'.format(valid_pred_min, valid_prec)
+            print('Validation precision increased({:.6f} --> {:.6f}).  Saving model ...'.format(
+            valid_pred_min,
+            valid_prec))
+            with open('log/logs_frc_0629.txt', 'a') as f :
+                f.write(line_3)
+                f.write('\r\n')
+            torch.save(model.state_dict(), f'fasterrcnn_epo{epo}_fpn.pth')
+            valid_pred_min = valid_prec
+        line_4 = f"Epoch #{epo} train_loss: {loss_hist.value}"
+        print(f"Epoch #{epo} train_loss: {loss_hist.value}")
+        with open('log/logs_frc_0629.txt', 'a') as f :
+            f.write(line_4)
+            f.write('\r\n')
+
+if __name__ == "__main__":
+    train()
